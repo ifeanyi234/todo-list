87b0dc6712ace1880b94df3e75a09ba4e9251d7c
sabzyhacks <ei711283@gmail.com>
2026-07-19 02:10:29 +0100

    tasks DOM insertion added

--- script.js
+++ script.js
@@ -2,12 +2,22 @@
 const input = document.querySelector("#taskInput");
 const addBtn = document.querySelector("#addBtn");
 const error = document.querySelector(".error");
+const listContainer = document.querySelector("#taskList");
 
-const task = [];
+const tasks = [];
 
 const addTask = function (taskValue) {
-  task.push(taskValue);
+  tasks.push(taskValue);
 };
+
+const createEl = function () {
+  const newEl = document.createElement("li");
+  newEl.classList.add("task-item");
+  listContainer.appendChild(newEl);
+  return newEl;
+};
+const addClass = function (el, className) {};
+const append = function (parentEl, el) {};
 
 addBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -17,8 +27,19 @@ addBtn.addEventListener("click", function (e) {
     return;
   } else {
     error.classList.remove("active");
+
+    // add task
     addTask(input.value);
-    console.log(task);
+
+    listContainer.innerHTML = "";
+
+    tasks.forEach((task, _, arr) => {
+      // create element
+      const newTaskItem = createEl();
+      newTaskItem.textContent = task;
+    });
+
+    console.log(tasks);
   }
 
   // reset
